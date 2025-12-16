7030a1d802caf72da1627c20f090a6415da19cc4
Fenil Kapadiya <fenilkapadiya26@gmail.com>
2025-12-16 13:47:43 +0530

    fix: correct upload middleware path for cover image route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -39,7 +39,7 @@ router
   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
   .route("/cover-image")
-  .get(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+  .get(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
 export default router;
